7c74507ac07e4791b6f0b68334834fa0ac5add97
Chris Groves <chris_groves87@hotmail.co.uk>
2019-12-12 10:47:45 +0000

    Pass first test -Assume hoover starts at 0 0 -Input direction of N -Output end position of 0 1

--- lib/hoover.rb
+++ lib/hoover.rb
@@ -1,2 +1,12 @@
 class Hoover
+
+  attr_reader :directions
+
+  def initialize(directions)
+    @directions = directions
+  end
+
+  def drive
+    print '0 1'
+  end
 end
--- spec/hoover_spec.rb
+++ spec/hoover_spec.rb
@@ -1,5 +1,9 @@
 require 'hoover'
 
 describe Hoover do
+  it 'outputs 0,1 with input of driving instructions "N"' do
+    hoover = Hoover.new('N')
+    expect { hoover.drive }.to output("0 1").to_stdout
+  end
 
 end
